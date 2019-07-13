9572ced5ba53eeef22d8d1b890a8150a2d2901ea
Jeyoung <jeyoung.park@salesforce.com>
2019-07-12 17:14:26 -0700

    fixed the error with getLabel function.

--- src/aura/viewer/BrowserApi.js
+++ src/aura/viewer/BrowserApi.js
@@ -2,6 +2,10 @@
  * Interact with the Browser Extensions API
  * Abstracted so we can normalize browser API differences in the WebExtensions protocol and chrome.
  */
+import stripJsonComments from "strip-json-comments";
+import fs from "fs";
+
+
 export default class BrowserApi {
     static eval(command) {
         return new Promise((resolve, reject) => {
@@ -11,7 +15,16 @@ export default class BrowserApi {
                 }
                 resolve(returnValue);
             });
+
         });
+    }
+
+    static getLabel(key) {
+        if(!labels.hasOwnProperty(key)) {
+            return `[${key}]`;
+        }
+
+        return labels[key].message;
     }
 }
 
@@ -23,4 +36,5 @@ function getBrowserApi() {
     return chrome;
 }
 
-
+const json = stripJsonComments(fs.readFileSync('_locales/en/messages.json', 'utf8')); 
+const labels = JSON.parse(json);
