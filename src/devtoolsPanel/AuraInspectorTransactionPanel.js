4fffcca799a576111d21e1b172da128f379211bb
Roche julien <julien.roche@salesforce.com>
2019-11-04 14:59:13 +0100

    chore(prettier): fix on transaction components (#14)
    Thx!

--- src/devtoolsPanel/AuraInspectorTransactionPanel.js
+++ src/devtoolsPanel/AuraInspectorTransactionPanel.js
@@ -183,7 +183,8 @@ export default function AuraInspectorTransactionPanel(devtoolsPanel) {
     function debounce(func, wait, immediate) {
         var timeout;
         return function() {
-            var context = this, args = arguments;
+            var context = this,
+                args = arguments;
             var later = function() {
                 timeout = null;
                 if (!immediate) func.apply(context, args);
